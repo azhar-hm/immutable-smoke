81184fea64ce209f43e605468ce3fa40288fd0a2
CodeSandbox <support@codesandbox.io>
2023-11-22 11:46:26 +0000

    Initial commit

---
+++ dist/typescript-sandbox.63012c5e.js
@@ -0,0 +1,362 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"src/typescript-sandbox.ts":[function(require,module,exports) {
+"use strict";
+
+// TypeScript Playground
+// write, share and learn TypeScript.
+var anExampleVariable = "Hello World";
+var palindromeText = "race_car!";
+var sentence = "The quick brown fox jumped over zakarivony";
+var sentence2 = "I'm a little teapot";
+function reverseString(str) {
+  // const strArray = str.split("");
+  // const reversedStrArray = strArray.reverse();
+  // const reversedString = reversedStrArray.join("");
+  // return reversedString;
+  // return str.split("").reverse().join("");
+  var result = "";
+  for (var i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+  return result;
+}
+function isPalindrome(str) {
+  var reg = /[\W_]/g;
+  var smallStr = str.toLowerCase().replace(reg, "");
+  // console.log('smallStr:: ', smallStr);
+  var reverseStr = reverseString(smallStr);
+  return reverseStr === smallStr;
+}
+function factorialize(num) {
+  var result = BigInt(1);
+  for (var i = num; i > 0; i--) {
+    result *= BigInt(i);
+  }
+  return result;
+}
+function findLongestWord(str) {
+  var words = str.split(" ");
+  var longestWords = "";
+  for (var _i = 0, words_1 = words; _i < words_1.length; _i++) {
+    var word = words_1[_i];
+    if (word.length > longestWords.length) {
+      longestWords = word;
+    }
+  }
+  // console.log(str.split(" ").sort((a, b) => a > b ? -1 : 1)[0].length);
+  return longestWords.length;
+}
+function titleCase(str) {
+  var words = str.toLowerCase().split(" ");
+  // console.log(words);
+  for (var i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    // console.log(words[i]);
+  }
+
+  return words.join(" ");
+}
+function titleCase2(str) {
+  var titled = str.toLowerCase().split(" ").map(function (word) {
+    return word[0].toUpperCase + word.slice(1);
+  });
+  return titled.join(" ");
+}
+console.log("reverseString('" + anExampleVariable + "')\n" + reverseString(anExampleVariable));
+console.log("");
+console.log("isPalindrome('" + palindromeText + "')\n" + isPalindrome(palindromeText));
+console.log("");
+console.log("factorialize('" + 25 + ")\n" + factorialize(25));
+console.log("");
+console.log("findLongestWord('" + sentence + ")\n" + findLongestWord(sentence));
+console.log("");
+console.log("titleCase2('" + sentence2 + ")\n" + titleCase2(sentence2));
+},{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "32847" + '/');
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      });
+
+      // Enable HMR for CSS by default.
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
+      }
+    }
+    if (data.type === 'reload') {
+      ws.close();
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+  if (overlay) {
+    overlay.remove();
+  }
+}
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID;
+
+  // html encode message and stack trace
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+  var parents = [];
+  var k, d, dep;
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+  return parents;
+}
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+  if (checkedAssets[id]) {
+    return;
+  }
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+    return true;
+  }
+}
+},{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","src/typescript-sandbox.ts"], null)
+//# sourceMappingURL=/typescript-sandbox.63012c5e.js.map
---
+++ src/typescript-sandbox.ts
@@ -0,0 +1,81 @@
+// TypeScript Playground
+// write, share and learn TypeScript.
+const anExampleVariable = "Hello World";
+const palindromeText = "race_car!";
+const sentence = "The quick brown fox jumped over zakarivony";
+const sentence2 = "I'm a little teapot";
+
+function reverseString(str: string): string {
+  // const strArray = str.split("");
+  // const reversedStrArray = strArray.reverse();
+  // const reversedString = reversedStrArray.join("");
+  // return reversedString;
+  // return str.split("").reverse().join("");
+  let result = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+  return result;
+}
+
+function isPalindrome(str: string): boolean {
+  const reg = /[\W_]/g;
+  const smallStr = str.toLowerCase().replace(reg, "");
+  // console.log('smallStr:: ', smallStr);
+  const reverseStr = reverseString(smallStr);
+  return reverseStr === smallStr;
+}
+
+function factorialize(num: number): BigInt {
+  let result = BigInt(1);
+  for (let i = num; i > 0; i--) {
+    result *= BigInt(i);
+  }
+  return result;
+}
+
+function findLongestWord(str: string): number {
+  const words: string[] = str.split(" ");
+  let longestWords: string = "";
+  for (let word of words) {
+    if (word.length > longestWords.length) {
+      longestWords = word;
+    }
+  }
+  // console.log(str.split(" ").sort((a, b) => a > b ? -1 : 1)[0].length);
+  return longestWords.length;
+}
+
+function titleCase(str: string): string {
+  const words: string[] = str.toLowerCase().split(" ");
+  // console.log(words);
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    // console.log(words[i]);
+  }
+  return words.join(" ");
+}
+
+function titleCase2(str: string): string {
+  let titled = str
+    .toLowerCase()
+    .split(" ")
+    .map((word: string) => {
+      return word[0].toUpperCase + word.slice(1);
+    });
+  return titled.join(" ");
+}
+
+console.log(
+  `reverseString('${anExampleVariable}')\n` + reverseString(anExampleVariable),
+);
+console.log(``);
+console.log(
+  `isPalindrome('${palindromeText}')\n` + isPalindrome(palindromeText),
+);
+console.log(``);
+console.log(`factorialize('${25})\n` + factorialize(25));
+console.log(``);
+console.log(`findLongestWord('${sentence})\n` + findLongestWord(sentence));
+console.log(``);
+console.log(`titleCase2('${sentence2})\n` + titleCase2(sentence2));
